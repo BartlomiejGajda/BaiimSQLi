2be759151915cff44e2eac56a69c97b2664e53cf
BartlomiejGajda <bgajda@student.agh.edu.pl>
2023-01-09 17:19:01 +0100

    improvements for easier sql injection

--- app/src/main/java/com/example/baiimsqli/DatabaseHelper.java
+++ app/src/main/java/com/example/baiimsqli/DatabaseHelper.java
@@ -4,10 +4,11 @@ import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
-import android.database.sqlite.SQLiteException;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class DatabaseHelper extends SQLiteOpenHelper {
 
@@ -52,16 +53,30 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         try {
             Cursor cursor = sqLiteDatabase.rawQuery("select * from users where username='" + username + "' and password= '" + password + "'", null);
             return cursor.getCount() > 0;
-        } catch (Exception e) {return false;}
+        } catch (Exception e) {sqLiteDatabase.close();return false;}
     }
+
+    public Boolean checkadmin(String username, String password) {
+        SQLiteDatabase sqLiteDatabase = this.getWritableDatabase();
+        //Cursor cursor = sqLiteDatabase.rawQuery("select * from users where username=? and password=?", new String[] {username, password});
+        try {
+            Cursor cursor = sqLiteDatabase.rawQuery("select admin from users where username='" + username + "' and password= '" + password + "'", null);
+            Log.i("ArrayList", String.valueOf(cursor.getInt(0)));
+            return cursor.getInt(0) == 1;
+        } catch (Exception e) {sqLiteDatabase.close();return false;}
+
+    }
+
+
 
     public String [] getMoviesList(){
         SQLiteDatabase sqLiteDatabase = this.getWritableDatabase();
-        String [] mydata;
+        String [] mydata = new String[0];
         ArrayList<String> array = new ArrayList<>();
         //Inside the method you've read the cursor, loop through it and add those item to array
         String sql="SELECT * FROM movies";
         //execute SQL
+        try {
         Cursor cr = sqLiteDatabase.rawQuery(sql, null);
         cr.moveToFirst();//cursor pointing to first row
         mydata = new String[cr.getCount()];//create array string based on numbers of row
@@ -70,26 +85,35 @@ public class DatabaseHelper extends SQLiteOpenHelper {
             mydata[i] = cr.getString(0);//insert new stations to the array list
             //Log.i("ArrayList",mydata[i]);
             i++;
-        }while(cr.moveToNext());
+        }while(cr.moveToNext());} catch (Exception e){
+            Log.e("ERR","exception",e);
+        };
         //Finally Set the adapter to AutoCompleteTextView like this,
         //ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
         //        android.R.layout.simple_dropdown_item_1line, mydata);
         //populate the list to the AutoCompleteTextView controls
+        sqLiteDatabase.close();
         return mydata;
     }
 
-    public String [] getTitle(String title){
+    public ArrayList<HashMap<String, String>> getTitle(String title){
         SQLiteDatabase sqLiteDatabase = this.getWritableDatabase();
-        Cursor cursor = sqLiteDatabase.rawQuery("select * from movies where title='" + title +  "'", null);
-        String [] mydata;
-        cursor.moveToFirst();
-        mydata = new String[cursor.getCount()];
-        int i=0;
-        do  {
-            mydata[i] = cursor.getString(0);//insert new stations to the array list
-            //Log.i("ArrayList",mydata[i]);
-            i++;
-        }while(cursor.moveToNext());
-        return mydata;
+        //Cursor cursor = sqLiteDatabase.rawQuery("select * from movies where title='" + title +  "'", null);
+        Cursor cursor = sqLiteDatabase.rawQuery("select * from movies where title=?", new String[] {title});
+        ArrayList<HashMap<String, String>> maplist = new ArrayList<HashMap<String, String>>();
+        if (cursor.moveToFirst()) {
+            do {
+                HashMap<String, String> map = new HashMap<String, String>();
+                for(int i=0; i<cursor.getColumnCount();i++)
+                {
+                    map.put(cursor.getColumnName(i), cursor.getString(i));
+                }
+
+                maplist.add(map);
+            } while (cursor.moveToNext());
+        }
+        sqLiteDatabase.close();
+        // return contact list
+        return maplist;
     }
 }
--- app/src/main/java/com/example/baiimsqli/HomeActivity.java
+++ app/src/main/java/com/example/baiimsqli/HomeActivity.java
@@ -4,13 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.TextView;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -27,8 +30,7 @@ public class HomeActivity extends AppCompatActivity {
         movieTextDisplay = findViewById(R.id.movieTextDisplay);
         movieSearchButton = findViewById(R.id.movieSearchButton);
         databaseHelper = new DatabaseHelper(this);
-
-
+        movieTextDisplay.setMovementMethod(new ScrollingMovementMethod());
 
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_dropdown_item_1line, databaseHelper.getMoviesList());
         movieSearchView = (AutoCompleteTextView) findViewById(R.id.movieSearchTextView);
@@ -39,8 +41,8 @@ public class HomeActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 String title = movieSearchView.getText().toString();
-                String [] data = databaseHelper.getTitle(title);
-                movieTextDisplay.setText(Arrays.toString(data));
+                ArrayList<HashMap<String, String>> data = databaseHelper.getTitle(title);
+                movieTextDisplay.setText(Arrays.toString(data.toArray()));
             }
         });
 
